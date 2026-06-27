ecb94c32ad27afa19e9b8601e3e6ecbcfc5bc535
Kijani Evans <builtbyke@gmail.com>
2026-06-26 22:15:34 -0400

    Update Detect-Wallpaper.ps1

--- scripts/powershell/Detect-Wallpaper.ps1
+++ scripts/powershell/Detect-Wallpaper.ps1
@@ -5,7 +5,7 @@
 
 $registryPath = "HKCU:\Control Panel\Desktop\"
 $value = (Get-ItemProperty -Path $registryPath -Name WallPaper -ErrorAction SilentlyContinue).WallPaper
-$wallpaperPath = "C:\Users\Public\Documents\Branded Wallpapers\new_wallpaper.jpg"
+$wallpaperPath = "C:\Users\Public\Documents\Branded Wallpapers\oshh-desktop-wallpaper-1920x1080.png"
 
 if ($value -eq $wallpaperPath) {
     Write-Output "Wallpaper is a match"
